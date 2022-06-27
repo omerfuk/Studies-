84607ee14dd0924445d1692ce64d1a46fe1e0003
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-27 14:31:50 +0300

    .

--- BirthdayNoteApp/ViewController.swift
+++ BirthdayNoteApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        guard let myName = storedName as? String else {return}
+        nameLabel.text = myName
+        guard let myBirthday = storedBirthday as? String else {return}
+        birthdayLabel.text = myBirthday
+
     }
 
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text, forKey: "birthday")
+        
 
+        
+        nameLabel.text = "Name : \(nameTextField.text!)"
+        birthdayLabel.text = "Birthday : \(birthdayTextField.text!)"
+    }
+    
 }
 
