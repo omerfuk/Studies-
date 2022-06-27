75c861a74a976dee558e9b7c76e125288c66f6d3
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-27 14:37:06 +0300

    User Defaults practice

--- BirthdayNoteApp/ViewController.swift
+++ BirthdayNoteApp/ViewController.swift
@@ -36,6 +36,21 @@ class ViewController: UIViewController {
         nameLabel.text = "Name : \(nameTextField.text!)"
         birthdayLabel.text = "Birthday : \(birthdayTextField.text!)"
     }
+    @IBAction func deleteButtonClicked(_ sender: Any) {
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if (storedName as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "name")
+            nameLabel.text = "Name :"
+        }
+        
+        if (storedBirthday as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            birthdayLabel.text = "Birthday : "
+        }
+    }
     
 }
 
